2c32193077c377fa62275573dd44ad9d1cd03ff6
doubleboom <512316614@qq.com>
2023-08-28 14:44:38 +0800

    Initial Commit

---
+++ newHelloWorld/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  newHelloWorld
+//
+//  Created by leee on 2023/8/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ newHelloWorld/newHelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  newHelloWorldApp.swift
+//  newHelloWorld
+//
+//  Created by leee on 2023/8/28.
+//
+
+import SwiftUI
+
+@main
+struct newHelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
